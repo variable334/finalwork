1fb0c83c1c212dae18e6db17fea455dcf56c63f2
choper328 <nelco2850@gmail.com>
2024-06-16 15:45:05 +0200

    we created an array of strings and a method that returns the number of elements with the parameter <=3

--- finalwork/Program.cs
+++ finalwork/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// string []str = {"1234","1567","-2","computer science"};
+
+// string[] str = { "Russia", "Denmark", "Kazan" };
+
+
+string[] str = { "Hello", "2", "world", ":-)" };
+
+
+
+
+
+
+
+int Position (string[]str)
+{
+   int count = 0;
+   for (int i = 0; i < str.Length; i++)
+   {
+      if(str[i].Count()<=3)
+      {
+         count ++;
+      }
+   }
+
+   return count;
+   
+}
